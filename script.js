47d7133c958397381dbfb3212c4203716f90c2cc
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-04-29 21:57:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
+// select the container element that holds the buttons
 let buttons = document.querySelector('.keys');
+
+// select all the buttons inside the container element
 let btn = document.querySelectorAll('span');
+
+// select the element that displays the input and output
 let value = document.getElementById('value');
 
-let hasCleared = true; // flag to indicate if the user has cleared the input
+// initialize a flag to keep track of whether the input has been cleared or not
+let hasCleared = false;
 
+// loop through all the buttons and add a click event listener to each one
 for (let index = 0; index < btn.length; index++) {
     btn[index].addEventListener("click", (event) => {
+        // get the clicked button from the event object
         const clickedButton = event.target;
         if (clickedButton.innerHTML === "=") {
-            value.innerHTML = calculate(value.innerHTML);
+            // if the "=" button is clicked, calculate the result and display it in the value element
+            const result = calculate(value.innerHTML);
+            value.innerHTML = parseFloat(result.toFixed(6)); // limit result to 5 decimal places
         } else if (clickedButton.innerHTML === "Clear") {
-            value.innerHTML = "0"; // set value to "0" instead of ""
-            hasCleared = true; // set flag to indicate input has been cleared
+            // if the "Clear" button is clicked, clear the input and set the flag
+            value.innerHTML = "0";
+            hasCleared = true;
         } else {
-            if (hasCleared) { // check if input has been cleared
-                value.innerHTML = ""; // clear the "0" before adding new input
-                hasCleared = false; // reset the flag
+            // if any other button is clicked, add its value to the input and clear the "0" if necessary
+            if (hasCleared) {
+                value.innerHTML = "";
+                hasCleared = false;
             }
             value.innerHTML += clickedButton.innerHTML;
         }
     })
 }
 
+// function to calculate the result of the input expression
 function calculate(expression) {
     return eval(expression);
 }
