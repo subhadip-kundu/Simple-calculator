c6f2e7ebb3652e95e9cf8d64e4390525845bfc12
Subhadip kundu <124190254+subhadip-kundu@users.noreply.github.com>
2023-08-16 19:27:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let buttons = document.querySelector('.keys');
 let btn = document.querySelectorAll('span');
 
 // select the element that displays the input and output
-let value = document.getElementById('value');
+let value = document.getElementsByClassName('value');
 
 // initialize a flag to keep track of whether the input has been cleared or not
 let hasCleared = true;
@@ -17,25 +17,25 @@ for (let index = 0; index < btn.length; index++) {
         const clickedButton = event.target;
         if (clickedButton.innerHTML === "=") {
             // if the "=" button is clicked, calculate the result and display it in the value element
-            const result = calculate(value.innerHTML);
+            const result = calculate(value[0].innerHTML);
             if (result.toString().length > 14) {
                 // if the result has more than 15 digits, convert it to exponential notation
-                value.innerHTML = parseFloat(result).toExponential(10);
+                value[0].innerHTML = parseFloat(result).toExponential(10);
             } else {
                 // otherwise, round the result to 5 decimal places and display it as a regular number
-                value.innerHTML = parseFloat(result.toFixed(5));
+                value[0].innerHTML = parseFloat(result.toFixed(5));
             }
         } else if (clickedButton.innerHTML === "Clear") {
             // if the "Clear" button is clicked, clear the input and set the flag
-            value.innerHTML = "0";
+            value[0].innerHTML = "0";
             hasCleared = true;
         } else {
             // if any other button is clicked, add its value to the input and clear the "0" if necessary
             if (hasCleared) {
-                value.innerHTML = "";
+                value[0].innerHTML = "";
                 hasCleared = false;
             }
-            value.innerHTML += clickedButton.innerHTML;
+            value[0].innerHTML += clickedButton.innerHTML;
         }
     })
 }
@@ -44,3 +44,27 @@ for (let index = 0; index < btn.length; index++) {
 function calculate(expression) {
     return eval(expression);
 }
+
+
+// Night
+
+const checkbox = document.querySelector('.toggle-state');
+const body = document.querySelector('body');
+const container = document.querySelector('.container')
+const toggle = document.querySelector('.toggle')
+
+
+checkbox.addEventListener('click', function() {
+  if (this.checked) {
+    body.style.backgroundColor = '#023529';
+    container.classList.add('container-dark');
+    toggle.classList.add('toggle-night')
+} else {
+    body.style.backgroundColor = '#fff';
+    container.classList.remove('container-dark');
+    toggle.classList.remove('toggle-night')
+  }
+});
+
+
+
